697166154e3744791ef03921d38beb70dce4b9da
Emily Dittmer <emilyedittmer@gmail.com>
2019-07-02 18:42:16 -0600

    Add mock-data and add star wars font"

---
+++ src/data/mock-data.js
@@ -0,0 +1,84 @@
+const mockData = {
+  films: [
+    {
+      "opening_crawl": "It is a period of civil war.\n\nRebel spaceships, striking\n\nfrom a hidden base, have won\n\ntheir first victory against\n\nthe evil Galactic Empire.\n\n\n\nDuring the battle, Rebel\n\nspies managed to steal secret\r\nplans to the Empire's\n\nultimate weapon, the DEATH\n\nSTAR, an armored space\n\nstation with enough power\n\nto destroy an entire planet.\n\n\n\nPursued by the Empire's\n\nsinister agents, Princess\n\nLeia races home aboard her\n\nstarship, custodian of the\n\nstolen plans that can save her\n\npeople and restore\n\nfreedom to the galaxy....",
+      "release_date": "1977-05-25",
+      "title": "A New Hope"
+    },
+    {
+      "opening_crawl": "Turmoil has engulfed the\r\nGalactic Republic. The taxation\r\nof trade routes to outlying star\r\nsystems is in dispute.\r\n\r\nHoping to resolve the matter\r\nwith a blockade of deadly\r\nbattleships, the greedy Trade\r\nFederation has stopped all\r\nshipping to the small planet\r\nof Naboo.\r\n\r\nWhile the Congress of the\r\nRepublic endlessly debates\r\nthis alarming chain of events,\r\nthe Supreme Chancellor has\r\nsecretly dispatched two Jedi\r\nKnights, the guardians of\r\npeace and justice in the\r\ngalaxy, to settle the conflict....", 
+      "release_date": "1999-05-19",
+      "title": "The Phantom Menace",
+    },
+    {
+      "opening_crawl": "Luke Skywalker has returned to\r\nhis home planet of Tatooine in\r\nan attempt to rescue his\r\nfriend Han Solo from the\r\nclutches of the vile gangster\r\nJabba the Hutt.\r\n\r\nLittle does Luke know that the\r\nGALACTIC EMPIRE has secretly\r\nbegun construction on a new\r\narmored space station even\r\nmore powerful than the first\r\ndreaded Death Star.\r\n\r\nWhen completed, this ultimate\r\nweapon will spell certain doom\r\nfor the small band of rebels\r\nstruggling to restore freedom\r\nto the galaxy...", 
+      "release_date": "1983-05-25", 
+      "title": "Return of the Jedi"
+    }
+  ],
+  people: [
+    {
+      "birth_year": "19BBY",
+      "eye_color": "blue", 
+      "height": "172",
+      "gender": "male",
+      "name": "Luke Skywalker"
+    },
+    {
+      "birth_year": "112BBY",
+      "eye_color": "yellow", 
+      "height": "167",
+      "gender": "n/a",
+      "name": "C-3PO"
+    },
+    {
+      "birth_year": "41.9BBY",
+      "eye_color": "yellow", 
+      "height": "202",
+      "gender": "male",
+      "name": "Darth Vader"
+    }
+  ],
+  vehicles: [
+    {
+      "name": "Sand Crawler",
+      "model": "Digger Crawler",
+      "vehicle_class": "wheeled",
+      "passengers": "30"
+    },
+    {
+      "name": "T-16 skyhopper",
+      "model": "T-16 skyhopper",
+      "vehicle_class": "repulsorcraft",
+      "passengers": "1"
+    },
+    {
+      "name": "X-34 landspeeder",
+      "model": "X-34 landspeeder",
+      "vehicle_class": "repulsorcraft",
+      "passengers": "1"
+    }
+  ],
+  planets: [
+    {
+      "name": "Alderaan",
+      "terrain": "grasslands, mountains",
+      "diameter": "12500",
+      "population": "2000000000"
+    },
+    {
+      "name": "Yavin IV",
+      "terrain": "jungle, rainforests",
+      "diameter": "10200",
+      "population": "1000"
+    },
+    {
+      "name": "Hoth",
+      "terrain": "frozen",
+      "diameter": "7200",
+      "population": "unknown"
+    }
+  ]
+}
+
+export default mockData;
