2f65eb7d1ec49a650dbc0eb0f78a3dcc49c04dba
Emily Dittmer <emilyedittmer@gmail.com>
2019-07-06 13:43:51 -0600

    Start adding router with catalog component

---
+++ src/components/Catalog/Catalog.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Catalog.scss';
+import { Link } from 'react-router-dom';
+
+const Catalog = ({ data }) => {
+  console.log(data)
+  return(
+    <h2>Catelog Start</h2>
+  )
+}
+
+export default Catalog;
