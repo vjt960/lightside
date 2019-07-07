5afd6bc6a3f38d4f3d5d5a90a9644e4062555d1a
Vinton Teo <taulago@ymail.com>
2019-07-07 10:03:09 -0600

    Separate each sentence in crawl into <p> elements. Include episode id in crawl. Refactor crawl styling.

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import './Showcase.scss';
+import Header from '../Header/Header.js';
 
 const Showcase = (props) => {
   const { films } = props;
   const randomIndex = Math.floor(Math.random() * Math.floor(films.length))
-  let movie = films[randomIndex]
+  const movie = films[randomIndex]
+  const crawls = movie.openingCrawl
+    .split('.').map(line => {
+      return (
+        <p>{line}</p>
+      )
+    })
 
   return (
-    <div className='fade Showcase'>
-      <section className='star-wars'>
-        <div className='crawl'>
-          <div className='title'>
-            <h3>{movie.title}</h3>
-            <h4>{movie.releaseDate}</h4>
-            <p>{movie.openingCrawl}</p>
+    <div className='showcase'>
+      <div className="fade"></div>
+      <section className="star-wars">
+        <div className="crawl">
+          <div className="title">
+            <p>{`Episode ${movie.episode}`}</p>
+            <h1>{movie.title}</h1>
           </div>
-        </div>
+          { crawls }
+         </div>
       </section>
     </div>
+
+
+    // <div className='fade showcase'>
+    //   <Header />
+    //   <section className='star-wars'>
+    //     <div className='crawl'>
+    //       <div className='title'>
+    //         <h3>{movie.title}</h3>
+    //         <h4>{movie.releaseDate}</h4>
+    //         { crawls }
+    //       </div>
+    //     </div>
+    //   </section>
+    // </div>
   )
 }
 
