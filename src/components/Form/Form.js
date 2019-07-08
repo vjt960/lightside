5e801b2d2b0911a71409e164d004cbe882af0a38
Vinton Teo <taulago@ymail.com>
2019-07-08 17:22:01 -0600

    Change className of navLinks to nav-link; Style component
    Co-Authored-by: Emily Dittmer <emilyedittmer@gmail.com>

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Form.scss';
 
-const Form = () => {
-
-    return(
-      <form className='buttons'>
-        <button><NavLink to='/people' className='button'>people</NavLink></button>
-        <button><NavLink to='/planets' className='button'>Planets</NavLink></button>
-        <button><NavLink to='/vehicles' className='button'>vehicles</NavLink></button>
-        <button><NavLink to='/favorites' className='button favorite-btn'>favorites</NavLink></button>
-      </form>
-    )
+const Form = (props) => {
+  const { counter } = props;
+  return(
+    <form className='category-form'>
+      <button type='button' className='category-btn'><NavLink to='/people' className='nav-link'>people</NavLink></button>
+      <button type='button' className='category-btn'><NavLink to='/planets' className='nav-link'>planets</NavLink></button>
+      <button type='button' className='category-btn'><NavLink to='/vehicles' className='nav-link'>vehicles</NavLink></button>
+      <button type='button' className='category-btn fave-btn'><NavLink to='/favorites' className='nav-link'>favorites <span className='counter'>{counter}</span></NavLink></button>
+    </form>
+  )
 }
 
 export default Form;
