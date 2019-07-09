33aac31f2e48afcfada602f8ac6f4aa52ace8e2e
Vinton Teo <taulago@ymail.com>
2019-07-09 13:12:17 -0600

    Structure, complete, and successfully pass tests for App component. Refactor apiCalls.
    Co-Authored-by: Emily Dittmer <emilyedittmer@gmail.com>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import {fetchAPI as data} from '../../utils/apiCalls';
-import Loader from '../Loader/Loader';
-import Showcase from '../Showcase/Showcase';
-import Form from '../Form/Form';
-import Catalog from '../Catalog/Catalog';
-import './App.scss';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { fetchAPI } from "../../utils/apiCalls";
+import Loader from "../Loader/Loader";
+import Showcase from "../Showcase/Showcase";
+import Form from "../Form/Form";
+import Catalog from "../Catalog/Catalog";
+import "./App.scss";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       isLoading: true,
-      error: '', 
-      category: '',
-      films: [], 
-      people: [], 
-      planets: [], 
-      vehicles: [], 
+      error: "",
+      category: "",
+      films: [],
+      people: [],
+      planets: [],
+      vehicles: [],
       favorites: []
-    }
+    };
   }
 
   async componentDidMount() {
-    await data;
+    const data = await fetchAPI();
     const { films, people, planets, vehicles } = data;
     this.setState({ films, people, planets, vehicles, isLoading: false });
   }
@@ -40,77 +40,88 @@ class App extends Component {
     } else {
       this.removeFavorite(clonedState[index].name);
     }
-  }
+  };
 
   addFavorite = obj => {
-    console.log('adding favorite')
-    this.setState({ favorites: [...this.state.favorites, obj]});
-  }
+    console.log("adding favorite");
+    this.setState({ favorites: [...this.state.favorites, obj] });
+  };
 
   removeFavorite = name => {
-    console.log('removing favorite')
+    console.log("removing favorite");
     const { favorites } = this.state;
     const filteredFaves = favorites.filter(obj => {
       return obj.name !== name;
     });
     this.setState({ favorites: filteredFaves });
-  }
+  };
 
-
-  
   render() {
-
     const { films, people, planets, vehicles, favorites } = this.state;
 
     return (
-
-      <div className='App'>
-        {
-          this.state.isLoading ? <Loader /> 
-          : <Showcase key={'films'} films={films} />
-        }
+      <div className="App">
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <Showcase key={"films"} films={films} />
+        )}
         <Form counter={favorites.length} />
-        <Route exact path='/' render={() => 
-          <Catalog 
-            key={'home'} 
-            counter={1} 
-            data={[]} 
-          />} 
+        <Route
+          exact
+          path="/"
+          render={() => <Catalog key={"home"} counter={1} data={[]} />}
         />
-        <Route exact path='/people' render={() => 
-          <Catalog 
-            toggle={this.toggleFavorite} 
-            key={'people'} 
-            counter={people.length} 
-            data={people} 
-          />} 
+        <Route
+          exact
+          path="/people"
+          render={() => (
+            <Catalog
+              toggle={this.toggleFavorite}
+              key={"people"}
+              counter={people.length}
+              data={people}
+            />
+          )}
         />
-        <Route exact path='/planets' render={() => 
-          <Catalog 
-            toggle={this.toggleFavorite} 
-            key={'planets'} 
-            counter={planets.length} 
-            data={planets} 
-          />} 
+        <Route
+          exact
+          path="/planets"
+          render={() => (
+            <Catalog
+              toggle={this.toggleFavorite}
+              key={"planets"}
+              counter={planets.length}
+              data={planets}
+            />
+          )}
         />
-        <Route exact path='/vehicles' render={() => 
-          <Catalog 
-            toggle={this.toggleFavorite} 
-            key={'vehicles'} 
-            counter={vehicles.length} 
-            data={vehicles} 
-          />} 
+        <Route
+          exact
+          path="/vehicles"
+          render={() => (
+            <Catalog
+              toggle={this.toggleFavorite}
+              key={"vehicles"}
+              counter={vehicles.length}
+              data={vehicles}
+            />
+          )}
         />
-        <Route exact path='/favorites' render={() => 
-          <Catalog 
-            toggle={this.toggleFavorite} 
-            key={'favorites'} 
-            counter={favorites.length} 
-            data={favorites} 
-          />} 
+        <Route
+          exact
+          path="/favorites"
+          render={() => (
+            <Catalog
+              toggle={this.toggleFavorite}
+              key={"favorites"}
+              counter={favorites.length}
+              data={favorites}
+            />
+          )}
         />
       </div>
-    )
+    );
   }
 }
 
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,39 +1,42 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-import mockData from '../../data/mock-data';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-describe('App', () => {
-
-  let fetchData;
-  let data;
+describe("App", () => {
   let wrapper;
 
   beforeEach(() => {
-    //
+    wrapper = shallow(<App />);
+    wrapper.setState({
+      people: [
+        { name: "jev", favorited: false },
+        { name: "Anneke", favorited: true }
+      ],
+      favorites: [{ name: "Anneke", favorited: true }]
+    });
   });
 
-  it('should match snapshot', () => {
+  it("should match snapshot", () => {
     expect(wrapper).toMatchSnapshot();
-  })
-
-  it('calls fetch with the right data', () => {
-    //
   });
 
-  it('should update state with the resolved fetch data', () => {
-    //
+  it("should be able to toggle favorited objects", () => {
+    wrapper.instance().toggleFavorite("people", "jev");
+    expect(wrapper.state("people")[1].favorited).toEqual(true);
   });
 
-  it('should be able to toggle favorited objects', () => {
-    //
+  it("should add objects to favorites", () => {
+    const expected = [
+      { name: "Anneke", favorited: true },
+      { name: "jev", favorited: true }
+    ];
+    wrapper.instance().toggleFavorite("people", "jev");
+    expect(wrapper.state("favorites")).toEqual(expected);
   });
 
-  it('should add objects to favorites', () => {
-    //
+  it("should remove objects from favorites", () => {
+    const expected = [];
+    wrapper.instance().toggleFavorite("people", "Anneke");
+    expect(wrapper.state("favorites")).toEqual(expected);
   });
-
-  it('should removed objects from favorites', () => {
-    //
-  });
-})
+});
